bc8999f1dc40aa53fcc3a29d83ef82757cf236a2
Diana Sofía Rosero <79727518+Dianarosero@users.noreply.github.com>
2024-04-15 20:42:33 -0500

    API_NASA

--- api-nasa.python/commets.py
+++ api-nasa.python/commets.py
@@ -1,28 +1,24 @@
 import requests
 import os
 
-def get_commet_data(api_key):
-    print("::: COMETS INFORMATION")
-    api_url = f"https://api.nasa.gov/neo/rest/v1/neo/3726712?api_key={api_key}"
+def get_coments_data (api_key) :
+    print ("Getting coments data")
+    api_url= f"https://api.nasa.gov/neo/rest/v1/neo/3726712?api_key={api_key}"
 
     try:
         response = requests.get(api_url)
         response.raise_for_status()
         data = response.json()
 
-        os.system('clear')
-        #Comet name
-        print(f"Comet name: {data['name']}")
-        #Absolute magnitude
-        #Estimated diameter max (KM)
-        #Estimated diameter min (KM)
-        #Estimated diameter max (FT)
-        #Estimated diameter min (FT)
-        #orbital_data:
-           #last_observation_date
-
+        print ("Coment name:", data [ "name"])
+        print ("apsolute magnitude: ", data ["absolute_magnitude_h"])
+        print ("Estimated diameter max (km): ", data["estimated_diameter"] ["kilometers"] ["estimated_diameter_max"])
+        print("Estimated diameter min (km): ", data["estimated_diameter"] ["kilometers"] ["estimated_diameter_min"])
+        print("Estimated diameter max (ft): ", data["estimated_diameter"]["feet"] ["estimated_diameter_max"])
+        print("Estimated diameter min (ft): ", data["estimated_diameter"]["feet"] ["estimated_diameter_min"])
+        print ("Last observation date: ", data["orbital_data"] ["last_observation_date"])
     except requests.exceptions.RequestException as e:
-        print(f"API error: {e}")
-#Main
-api_key_nasa = 'c9hkw2qN7iXHdixPD2OKlTMNTNBe0x1qFGxkABFo'
-get_commet_data(api_key_nasa)   
+        print (f"API ERROR{e}")
+
+api_key_nasa = "eEqVBWUM3eotzuCJ1jTkKpWsx0wZfjz45yhVvKfG"
+get_coments_data(api_key_nasa)
--- api-solar-system-python/api-galaxy.py
+++ api-solar-system-python/api-galaxy.py
@@ -1,32 +1,60 @@
-import requests #Let 'u get data from api-url
-import os #Let 'u execute os commands
+import requests
+import os
 
-def get_data():
-    os.system("clear") #Clear screen
-    print("::: SOLAR SYSTEM INFORMATION :::")
-    api_url = "https://api.le-systeme-solaire.net/rest/bodies/?filter%5B%5D=isComet"
+def get_bodies(body_type):
+    os.system("clear")  # Limpiar pantalla
+    print(f"::: INFORMACIÓN DE {body_type.upper()} :::")
+    api_url = f"https://api.le-systeme-solaire.net/rest/bodies/"
 
     try:
-        #Request to API
-        response = requests.get(api_url)
-        response.raise_for_status() #Read errors
-    
+        # Hacer la solicitud a la API con el filtro para el tipo de cuerpo celeste especificado
+        response = requests.get(api_url, params={"filter[]": f"bodyType,{body_type}"})
+        response.raise_for_status()  # Manejar errores
+
         data = response.json()
 
-        print("#### Main menu ####")
-        print("[1]. Planets")
-        print("[2]. Moons")
-        print("[3]. Stars")
-        print("[4]. Asteroid")
-        print("[5]. Comets")
-        print("[6]. Exit")
-        opt = input("::: Press any option: ")
+        if "bodies" in data:
+            for body in data["bodies"]:
+                if body.get("bodyType") == body_type.capitalize():  # Filtrar por tipo de cuerpo celeste
+                    print("Nombre en inglés:", body["englishName"])
+                    print("Gravedad:", body["gravity"])
+                    print("Inclinación:", body["inclination"])
+                    print("¿Es un planeta?", body["isPlanet"])
+                    print("\n")
+        else:
+            print(f"No se encontraron datos de {body_type}.")
 
-        return data
     except requests.exceptions.RequestException as e:
-        print(f"API error {e}") #=> print("API error ",e)
+        print(f"Error al realizar la solicitud a la API: {e}")
 
+def main():
+    while True:
+        print("#### MENÚ PRINCIPAL ####")
+        print("[1]. Mostrar información de Planetas")
+        print("[2]. Mostrar información de Lunas")
+        print("[3]. Mostrar información de Estrellas")
+        print("[3]. Mostrar información de Ateroides")
+        print("[4]. Mostrar información de Cometas")
+        print("[5]. Salir")
 
-#Main
-info = get_data()
-print(info)
+        opt = input("::: Elija una opción: ")
+
+        if opt == '6':
+            print("Saliendo del programa...")
+            break
+        
+        if opt == '1':
+            get_bodies("Planet")
+        elif opt == '2':
+            get_bodies("Moon")
+        elif opt == '3':
+            get_bodies("Star")
+        elif opt == '4':
+            get_bodies("Comet")
+        elif opt == '5':
+            get_bodies("Asteroid")    
+        else:
+            print("Opción no válida. Por favor, elija de nuevo.")
+
+if __name__ == "__main__":
+    main()
